ff6bb3f761272f09edbbdf0cbcb09c2eda54caa1
rasasasa <41230344+rasasasa@users.noreply.github.com>
2019-12-13 17:23:25 +0700

    te

--- application/models/Supplier_m.php
+++ application/models/Supplier_m.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Supplier_m extends CI_Model {
-//test
+
     public function get($id = null)
 	{
 		$this->db->from('user');
